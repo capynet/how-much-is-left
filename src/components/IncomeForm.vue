0f15083cbd10821798b985e8595f916167a05c3b
Marcelo <marcelo@feedbackcompany.com>
2022-08-03 15:39:08 +0200

    Setting up basics.

---
+++ src/components/IncomeForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <label>
+  <input type="number" name="income-amount">
+  </label>
+ <input type="submit">
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
