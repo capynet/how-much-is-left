008ebc99941f82c1e3ec5128a0916d12f8ba95b2
Marcelo <marcelo@feedbackcompany.com>
2022-08-04 14:22:26 +0200

    - Added SASS. - Creating basic form.

--- src/components/IncomeForm.vue
+++ src/components/IncomeForm.vue
@@ -1,20 +1,53 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps<{ msg: string }>();
+// const submit = function () {
+//
+// };
 
-const count = ref(0);
+const incomeAmount = ref();
+const origin = ref("ueExempt");
 </script>
 
 <template>
-  <label>
-    <input type="number" name="income-amount" />
-  </label>
+  <p>Amount</p>
+  <div class="form-item">
+    <label>
+      <input
+        type="number"
+        id="income-amount"
+        name="income-amount"
+        v-model.number="incomeAmount"
+        required
+      />
+    </label>
+  </div>
+
+  <fieldset class="form-item">
+    <legend>Origin</legend>
+    <div class="form-item">
+      <input type="radio" id="spain" value="spain" v-model="origin" />
+      <label for="spain">Spain</label>
+    </div>
+
+    <div class="form-item">
+      <input type="radio" id="ue-exempt" value="ueExempt" v-model="origin" />
+      <label for="ue-exempt">UE exempt</label>
+    </div>
+  </fieldset>
+
+  <div class="brief form-item">
+    <p><strong>Amount: </strong>{{ incomeAmount }}</p>
+    <p><strong>Origin: </strong>{{ origin }}</p>
+  </div>
+
   <input type="submit" />
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style scoped lang="scss">
+.brief {
+  background: #369;
+  color: white;
+  padding: 10px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import IncomeForm from "@/components/IncomeForm.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <IncomeForm />
   </main>
 </template>
